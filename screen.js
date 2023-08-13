e0ce609578d665126324705c8edeb769c91c0cb1
shivam5676 <ssultanpura9@gmail.com>
2023-08-13 01:16:58 -0700

    serve created on 4000port with data as response

--- screen.js
+++ screen.js
@@ -1,3 +1,9 @@
-console.log("hello world")
-const fs=require("fs")
-fs.writeFileSync("screen.txt","hello world")
+
+
+const http=require("http")
+const server=http.createServer((req,res)=>{
+    console.log(req)
+ res.writeHead(200,{ 'Content-Type': 'text/plain'})
+ res.end("myname is shivam singh ")
+})
+server.listen(4000)
