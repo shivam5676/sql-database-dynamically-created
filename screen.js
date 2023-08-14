febf04b27649d2031cd45b57157bbf9d063795e2
shivam5676 <ssultanpura9@gmail.com>
2023-08-14 01:46:07 -0700

    rotes added for node js

--- screen.js
+++
@@ -1,9 +0,0 @@
-
-
-const http=require("http")
-const server=http.createServer((req,res)=>{
-    console.log(req)
- res.writeHead(200,{ 'Content-Type': 'text/plain'})
- res.end("myname is shivam singh ")
-})
-server.listen(4000)
