7378af96c5abb0ef897b9d7020447a11fa928e16
shivam5676 <ssultanpura9@gmail.com>
2023-08-15 21:35:11 -0700

    getting data from input field and consoling that data in my console bar

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const http = require("http");
+const bodyParser = require("body-parser");
 
-const express=require('express');
-const app=express();
+const express = require("express");
 
-app.use((req,res,next)=>{
-    console.log("in the middleware 1")
-    next();
-})
-app.use((req,res,next)=>{
-    console.log("in the middleware 2")
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false })); 
 
-const server = http.createServer(app);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add product</button></form>'
+  );
+});
 
-server.listen(3000)
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use('/', (req, res, next) => {
+  res.send("<h1>hello from express</h1>");
+});
+
+app.listen(3000);
